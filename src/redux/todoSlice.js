5b36f337c1e953abb2df886387a2877d55eb525e
OksaGurzhyi <gurzhiy.oksa@gmail.com>
2023-08-12 15:24:27 +0200

    deleteTodo

--- src/redux/todoSlice.js
+++ src/redux/todoSlice.js
@@ -1,28 +1,30 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { nanoid } from "nanoid";
+import { createSlice } from '@reduxjs/toolkit';
+import { nanoid } from 'nanoid';
 
 const todoSlice = createSlice({
-    name: 'todos',
-    initialState: {
-        items: [],
+  name: 'todos',
+  initialState: {
+    items: [],
+  },
+  reducers: {
+    addTodo: {
+      reducer(state, action) {
+        state.items.push(action.payload);
+      },
+      prepare(text) {
+        return {
+          payload: {
+            text,
+            id: nanoid(),
+          },
+        };
+      },
     },
-    reducers: {
-        addTodo: {
-            reducer(state, action) {
-                state.items.push(action.payload)
-            },
-            prepare(text) {
-                return {
-                    payload: {
-                        text,
-                        id: nanoid()
-                    }
-                }
-            }
-        },
-        deleteTodo(state, action) { },
-    }
-})
+    deleteTodo(state, action) {
+      state.items = state.items.filter(el => el.id !== action.payload);
+    },
+  },
+});
 
 export const { addTodo, deleteTodo } = todoSlice.actions;
 export const todoReducer = todoSlice.reducer;
