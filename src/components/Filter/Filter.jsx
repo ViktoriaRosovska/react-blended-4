f8248fd5a72d8c4b3c9e18d2b5fd35c12a9d6767
Viktoria Rosovska <torimeister@gmail.com>
2023-08-15 23:36:53 +0300

    add persist

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,14 +1,15 @@
-import { useDispatch } from "react-redux"
-import { addFilter } from "redux/filterSlice"
+import { useDispatch, useSelector } from 'react-redux';
+import { addFilter } from 'redux/filterSlice';
+import { selectFilter } from 'redux/selectors';
 
 export const Filter = () => {
-  
-  const dispatch = useDispatch() 
-  
+  const dispatch = useDispatch();
   return (
-  <input type="text" placeholder = "Filter Todo"
-  onChange = {
-      (event) => dispatch(addFilter (event.target.value))
-  }/>
-
-)}
+    <input
+      type="text"
+      placeholder="Filter Todo"
+      onChange={event => dispatch(addFilter(event.target.value))}
+      value={useSelector(selectFilter)}
+    />
+  );
+};
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,37 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { todoReducer } from "./todoSlice";
-import { filterReducer } from "./filterSlice";
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { todoReducer } from './todoSlice';
+import { filterReducer } from './filterSlice';
 
+import storage from 'redux-persist/lib/storage';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+const rootReducer = combineReducers({
+  todos: todoReducer,
+  filter: filterReducer,
+});
+const persistConfig = {
+  key: 'todosApp',
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-    reducer: {
-        todos: todoReducer,
-filter: filterReducer
-    }
-})
+  reducer: persistedReducer,
+  middleware(getDefaultMiddleware) {
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    });
+  },
+});
+
+export const persistor = persistStore(store);
